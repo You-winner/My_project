414cb8c4dcc7b5b988c60d3953ca27f217b3e5d8
Your-winner <matvey.kozlov.1980@gmail.com>
2022-09-03 15:54:45 +0300

    Exam project

--- Exam 2.py
+++ Exam 2.py
@@ -77,7 +77,7 @@ print(u[id_longest])
 # Задание 8. Есть словарь песен группы Depeche Mode violator songsdict = { 'World in My Eyes': 4.76, 'Sweetest Perfection': 5.43, 'Personal Jesus': 4.56, 'Halo': 4.30, 'Waiting for the Night': 6.07, 'Enjoy the Silence': 4.6, 'Policy of Truth': 4.88, 'Blue Dress': 4.18, 'Clean': 5.68, }. Выведите общее время звучания всех песен. Создайте список песен, время звучаниях которых больше 5 минут Создайте новый словарь тех песен, в название которых состоит из одного слова.
 
 Depeche_Mode_violator_songsdict = { 'World in My Eyes': 4.76, 'Sweetest Perfection': 5.43, 'Personal Jesus': 4.56, 'Halo': 4.30, 'Waiting for the Night': 6.07, 'Enjoy the Silence': 4.6, 'Policy of Truth': 4.88, 'Blue Dress': 4.18, 'Clean': 5.68, }
-rtm = 1
+rtm = 0
 for key in Depeche_Mode_violator_songsdict:
     rtm = rtm + Depeche_Mode_violator_songsdict[key]
 print(rtm)
